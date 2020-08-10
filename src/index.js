9aafb24e90583fe7aae1372c9319482abcd7da06
Peter Ivanovskij <65976810+Ivanovskij-Peter@users.noreply.github.com>
2020-08-10 21:54:01 +0300

    добавил новое задание

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 import './styles.css';
+import menu from './menu.json';
+import itemTemplate from './templates/item.hbs';
+import AllItemTemplate from './templates/allitems.hbs';
+const listRef = document.querySelector('.js-menu');
+// const item = itemTemplate(menu);
+// listRef.insertAdjacentHTML('afterbegin', item);
+
+function generateMenu(menu) {
+  const item = menu.map(el => AllItemTemplate(el)).join('');
+  listRef.insertAdjacentHTML('afterbegin', item);
+}
+generateMenu(menu);
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+const btn = document.querySelector('.js-switch-input');
+const classBody = document.querySelector('body');
+btn.addEventListener('click', () => {
+  let darkTheme = btn.checked ? true : false;
+  localStorage.setItem('theme', darkTheme);
+  localStorage.getItem('theme') === 'true'
+    ? classBody.classList.add('dark-theme')
+    : classBody.classList.remove('dark-theme');
+});
+localStorage.getItem('theme') === 'true'
+  ? classBody.classList.add('dark-theme')
+  : classBody.classList.remove('dark-theme');
